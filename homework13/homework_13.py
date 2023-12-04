4cef843a73b3ec0163c8bef9258536a0103650b7
YurySubbota <plus375291421728@gmail.com>
2023-12-04 22:01:02 +0300

    it's work homework13

--- homework13/homework_13.py
+++ homework13/homework_13.py
@@ -6,32 +6,31 @@ class DataStorage:
         self.__path = path
         self.status = 'disconnected'
         self.content = None
-        self.__file = None
+        self._file = None
 
     @property
     def path(self):
         return self.__path
 
     def _create_storage(self):
-        with open(f'{self.path}', 'w') as self.__file:
-            json.dump([], self.__file)
-            return self.__file
+        with open(f'{self.path}', 'w') as self._file:
+            json.dump([], self._file)
+            return self._file
 
     def connect(self):
         try:
-            self.__file = open(f'{self.path}', 'r')
-            self.content = json.load(self.__file)
+            self._file = open(f'{self.path}', 'r')
+            self.content = json.load(self._file)
             self.status = 'connected'
         except FileNotFoundError:
             self._create_storage()
-            self.__file = open(f'{self.path}', 'r')
-            self.content = json.load(self.__file)
+            self._file = open(f'{self.path}', 'r')
+            self.content = json.load(self._file)
             self.status = 'connected'
 
     def disconnect(self):
         if self.status == 'connected':
-            print('self.__file in disconnect DataStorage', self.__file)
-            self.__file.close()
+            self._file.close()
             self.status = 'disconnected'
             print(f'File {self.path} is closed now')
         else:
@@ -42,40 +41,44 @@ class DataStorageWrite(DataStorage):
     def connect(self):
         if self.status == 'disconnected':
             try:
-                with open(f'{self.path}', 'r') as self.__file:
-                    self.content = json.load(self.__file)
-                self.__file = open(f'{self.path}', 'w')
+                with open(f'{self.path}', 'r') as self._file:
+                    self.content = json.load(self._file)
+                self._file = open(f'{self.path}', 'w')
                 self.status = 'connected'
             except FileNotFoundError:
                 self._create_storage()
-                with open(f'{self.path}', 'r') as self.__file:
-                    self.content = json.load(self.__file)
-                self.__file = open(f'{self.path}', 'w')
+                with open(f'{self.path}', 'r') as self._file:
+                    self.content = json.load(self._file)
+                self._file = open(f'{self.path}', 'w')
                 self.status = 'connected'
         else:
             print('File is connected. Before connect again please use disconnect')
 
-    def append_string(self, string_to_append):
+    def append(self, string_to_append):
         if self.status == 'connected':
             self.content.append(string_to_append)
-            print(self.content)
         else:
             print('File is disconnected. before append new string please use connect')
 
     def disconnect(self):
-        json.dump(self.content, self.__file)
-        print('self.__file in disconnect DataStorageWrite', self.__file)
+        json.dump(self.content, self._file)
         super().disconnect()
 
 
 storage1 = DataStorageWrite('s1.json')
 storage1.connect()
 print(storage1.content)
-storage1.append_string('hi')
-storage1.append_string('how')
-storage1.append_string('are')
-storage1.append_string('you')
+storage1.append('hi')
+storage1.append('how')
+storage1.append('are')
+storage1.append('you')
 storage1.disconnect()
 storage1.connect()
 print(storage1.content)
+storage1.disconnect()
+storage1.connect()
+storage1.append('i')
+storage1.append('am')
+storage1.append('fine')
+storage1.append('thanks')
 storage1.disconnect()
