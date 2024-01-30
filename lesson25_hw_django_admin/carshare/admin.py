db9cd98aad224bae02abf995b80c5b25576ca7e5
YurySubbota <plus375291421728@gmail.com>
2024-01-30 18:22:26 +0300

    not ready

--- lesson25_hw_django_admin/carshare/admin.py
+++ lesson25_hw_django_admin/carshare/admin.py
@@ -1,3 +1,50 @@
 from django.contrib import admin
+from carshare.models import Auto, AutoModel, AutoUser, AutoBrand
+from django.utils.safestring import mark_safe
+
 
 # Register your models here.
+
+
+@admin.register(Auto)
+class AutoAdmin(admin.ModelAdmin):
+    list_display = ('vin_code',)
+    list_filter = ('auto_model__brand', 'status')
+    search_fields = ('vin_code',)
+    ordering = ('auto_model__brand',)
+
+    def set_status_free(admin_model, request, queryset):
+        queryset.update(status='FREE')
+
+    def set_status_booked(admin_model, request, queryset):
+        queryset.update(status='INUSE')
+
+
+@admin.register(AutoModel)
+class AutoModelAdmin(admin.ModelAdmin):
+    list_display = ('count_of_auto', 'count_of_auto',)
+    list_filter = ('brand',)
+
+    def count_of_auto(self, instance):
+        count = Auto.objects.filter(auto_model__auto__vin_code=instance.vin_code).count()
+        return count
+
+    count_of_auto.short_description = 'Auto'
+
+
+class AutoModelInline(admin.TabularInline):
+    model = AutoModel
+
+
+@admin.register(AutoBrand)
+class AutoBrandAdmin(admin.ModelAdmin):
+    list_display = ('brand',)
+
+    inlines = [AutoModelInline]
+
+    readonly_fields = ('preview',)
+
+    def preview(self, instance: AutoBrand):
+        if not instance.image:
+            return mark_safe(f'<b>without logo</b>')
+        return mark_safe(f'<img src="{instance.image.url_image}" style="max-width: 25px">')
