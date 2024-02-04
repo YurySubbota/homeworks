4014da68878ee0009af2c1101c8329f11df74568
YurySubbota <plus375291421728@gmail.com>
2024-02-04 17:27:42 +0300

    cat news

--- lesson26_hw_catnews/catnews/urls.py
+++ lesson26_hw_catnews/catnews/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, re_path
-from .views import HomePageView
+from .views import HomePageView, NewsPageView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='homepage'),
+    path('news/<int:id>/', NewsPageView.as_view(), name='news_page',),
+
 
 ]
--- lesson26_hw_catnews/catnews/views.py
+++ lesson26_hw_catnews/catnews/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, DetailView
 from django.views import View
 from catnews.models import News, Comment
+
 
 # Create your views here.
 
@@ -10,10 +11,9 @@ class HomePageView(View):
     DEFAULT_NEWS_PICTURE = 'media/default.jpg'
     sort_by = 'pub_date'
     def get(self, request, *args, **kwargs):
-        self.sort_by = self.request.GET.get('sort')
-        order_by = self.request.GET.get('order')
+        self.sort_by = self.request.GET.get('sort', 'pub_date')
+        order_by = self.request.GET.get('order', 'desc')
         self.sort_by = self.sort_by if order_by == 'asc' else f'-{self.sort_by}'
-        print(self.sort_by)
         news = News.objects.order_by(self.sort_by)[:20]
         for new in news:
             desc_worlds_list = new.body.split(' ')[:10]
@@ -23,4 +23,14 @@ class HomePageView(View):
 
         return render(request, 'index.html',
                       context={'sort': self.sort_by, 'news_list': news, 'title': self.TITLE,
+                        'default_jpg': self.DEFAULT_NEWS_PICTURE})
+
+
+class NewsPageView(View):
+    DEFAULT_NEWS_PICTURE = 'media/default.jpg'
+    def get(self, request, id, *args, **kwargs):
+        news = News.objects.get(pk=id)
+        self.TITLE = news.title
+
+        return render(request, 'news_page.html', context={'news': news, 'title': self.TITLE,
                         'default_jpg': self.DEFAULT_NEWS_PICTURE})
